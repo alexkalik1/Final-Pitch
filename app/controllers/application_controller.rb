0d6a28ca5ae1c78eb6fbad577d5eb41ba16c0b96
Alexander Kalik <alexanderk1996@gmail.com>
2017-11-08 11:45:43 -0500

    adding logout option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  before_action :authorize
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def require_logged_in
+    return true if current_user
+
+    return redirect_to root_path
+  end
+
+  def authorize
+    redirect_to 'users#new' unless current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   #
   # post '/login' => 'sessions#create'
   # get '/login' => 'sessions#new'
-  # get '/logout' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
