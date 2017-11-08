dd46323dfc9630dca82b7e0b3432b464b04dd208
Kareem <reemy396@aol.com>
2017-11-07 19:52:15 -0500

    Added controllers and model relation.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108003235) do
+ActiveRecord::Schema.define(version: 20171108005000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20171108003235) do
     t.date "term"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_loans_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +42,5 @@ ActiveRecord::Schema.define(version: 20171108003235) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "loans", "users"
 end
