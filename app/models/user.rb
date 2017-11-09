5730c0364d7f3296da0524144341c10505e49589
pkbanks <pkbanks@gmail.com>
2017-11-08 19:24:20 -0500

    updated to 'borrowings' and 'lendings'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-	has_many :loans, class_name: "Loan", foreign_key: "borrower_id"
-	has_many :investments, class_name: "Loan", foreign_key: "lender_id"
+	has_many :borrowings, class_name: "Loan", foreign_key: "borrower_id"
+	has_many :lendings, class_name: "Loan", foreign_key: "lender_id"
 
   has_secure_password
   enum role: [:Lender, :Borrower, :Admin]
