ead540c2fd8092795ff411de53590fc083d39920
Kareem <reemy396@aol.com>
2017-11-10 18:31:48 -0500

    Pushing to switch branch, no major changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     return render action: 'new' unless @user.save
 
-    redirect_to 'root_path', notice: 'Created user'
+    redirect_to user_path(@user), notice: 'Created user'
   end
 
   # PATCH/PUT /users/1
